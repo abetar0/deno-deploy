bb9d0190236d7384a932da2c1cdfb377f852d146
Taro Abe <tarofuku827@gmail.com>
2021-03-31 22:23:07 +0900

    update

--- hello.ts
+++ hello.ts
@@ -18,40 +18,6 @@ function handleRequest(request) {
     });
   }
 
-  if (pathname.startsWith("/json")) {
-    // Use stringify function to convert javascript object to JSON string.
-    const json = JSON.stringify({
-      message: "Hello from Deno Deploy",
-    });
-
-    return new Response(json, {
-      headers: {
-        "content-type": "application/json; charset=UTF-8",
-      },
-    });
-  }
-
-  return new Response(
-    `<body
-      align="center"
-      style="font-family: Avenir, Helvetica, Arial, sans-serif; font-size: 1.5rem;"
-    >
-      <h1>Return JSON and/or HTML Example</h1>
-      <p>
-        <a href="/html">/html</a> - responds with HTML to the request.
-      </p>
-      <p>
-        <a href="/json">/json</a> - responds with JSON to the request.
-      </p>
-    </body>`,
-    {
-      headers: {
-        "content-type": "text/html; charset=UTF-8",
-      },
-    },
-  );
-}
-
 addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
